02d75eebf86e3966044ccd7a5d6cf32c9b3eb745
AbrahamHub <apple_abraham@hotmail.com>
2021-03-21 18:22:07 -0600

    Final version

--- js/view.js
+++ js/view.js
@@ -1,109 +1,113 @@
+import AddTodo from './components/add-todo.js';
+import Modal from './components/modal.js';
+import Filters from './components/filters.js';
+
 export default class View {
-    constructor() {
-      this.model = null;
-      this.titleInput = document.getElementById("title");
-      this.descriptionInput = document.getElementById("description");
-      this.addBtn = document.getElementById("add");
-      this.table = document.getElementById("table");
-      this.alert = document.querySelector(".alert");
-  
-      this.modalTitle = document.getElementById("modal-title");
-      this.modalDescription = document.getElementById("modal-description");
-      this.modalBtn = document.getElementById("modal-btn");
-      this.modalCompleted = document.getElementById("modal-completed");
-  
-      this.addListeners();
-    }
-  
-    addListeners() {
-      this.addBtn.onclick = () => this.addTodo();
-      this.modalBtn.onclick = () =>
-        this.editTodo(
-          parseInt(this.modalBtn.getAttribute("todo-id")),
-          this.modalTitle.value,
-          this.modalDescription.value,
-          this.modalCompleted.checked
-        );
-    }
-  
-    setModel(model) {
-      this.model = model;
-    }
-  
-    render() {
-      const todos = this.model.getTodos();
-      todos.forEach((todo) => this.createRow(todo));
-    }
-  
-    toggleModal(todo) {
-      this.modalTitle.value = todo.title;
-      this.modalDescription.value = todo.description;
-      this.modalBtn.setAttribute("todo-id", todo.id);
-    }
-  
-    editTodo(id, title, description, completed) {
-      this.model.editTodo(id, title, description, completed);
-      const row = document.getElementById(id);
-      row.children[0].innerText = title;
-      row.children[1].innerText = description;
-      row.children[2].children[0].checked = completed;
-      $("#modal").modal("toggle");
-    }
-  
-    toggleCompleted(id) {
-      this.model.toggleCompleted(id);
-    }
-  
-    removeTodo(id) {
-      this.model.removeTodo(id);
-      document.getElementById(id).remove();
-    }
-  
-    addTodo() {
-      if (!this.titleInput.value || !this.descriptionInput.value) {
-        this.alert.classList.remove("d-none");
-        this.alert.innerText = "Title and description are required!";
+  constructor() {
+    this.model = null;
+    this.table = document.getElementById('table');
+    this.addTodoForm = new AddTodo();
+    this.modal = new Modal();
+    this.filters = new Filters();
+    
+
+    this.addTodoForm.onClick((title, description) => this.addTodo(title, description));
+    this.modal.onClick((id, values) => this.editTodo(id, values));
+    this.filters.onClick((filters) => this.filter(filters));
+  }
+
+  setModel(model) {
+    this.model = model;
+  }
+
+  render() {
+    const todos = this.model.getTodos();
+    todos.forEach((todo) => this.createRow(todo));
+  }
+
+  filter(filters) {
+    const { type, words } = filters;
+    const [, ...rows] = this.table.getElementsByTagName('tr');
+    for (const row of rows) {
+      const [title, description, completed] = row.children;
+      let shouldHide = false;
+
+      if (words) {
+        shouldHide = !title.innerText.includes(words) && !description.innerText.includes(words);
+      }
+
+      const shouldBeCompleted = type === 'completed';
+      const isCompleted = completed.children[0].checked;
+
+      if (type !== 'all' && shouldBeCompleted !== isCompleted) {
+        shouldHide = true;
+      }
+
+      if (shouldHide) {
+        row.classList.add('d-none');
       } else {
-        const todo = this.model.addTodo(
-          this.titleInput.value,
-          this.descriptionInput.value
-        );
-        this.createRow(todo);
-        this.alert.classList.add("d-none");
+        row.classList.remove('d-none');
       }
     }
-  
-    createRow(todo) {
-      const row = this.table.insertRow();
-      row.setAttribute("id", todo.id);
-  
-      row.innerHTML = `
-        <td>${todo.title}</td>
-        <td>${todo.description}</td>
-        <td class="text-center">
-        </td>
-        <td class="text-right">
-        </td>
-      `;
-  
-      const checkbox = document.createElement("input");
-      checkbox.type = "checkbox";
-      checkbox.checked = todo.completed;
-      checkbox.onclick = () => this.toggleCompleted(todo.id);
-      row.children[2].appendChild(checkbox);
-  
-      const editBtn = document.createElement("button");
-      editBtn.classList.add("btn", "btn-primary", "mb-1");
-      editBtn.innerHTML = '<i class="fa fa-pencil"></i>';
-      editBtn.setAttribute("data-toggle", "modal");
-      editBtn.setAttribute("data-target", "#modal");
-      editBtn.onclick = () => this.toggleModal(todo);
-      row.children[3].appendChild(editBtn);
-  
-      const removeBtn = document.createElement("button");
-      removeBtn.classList.add("btn", "btn-danger", "ml-1", "mb-1");
-      removeBtn.innerHTML = '<i class="fa fa-trash"></i>';
-      removeBtn.onclick = () => this.removeTodo(todo.id);
-      row.children[3].appendChild(removeBtn);
-    }
   }
+
+  addTodo(title, description) {
+    const todo = this.model.addTodo(title, description);
+    this.createRow(todo);
+  }
+
+  toggleCompleted(id) {
+    this.model.toggleCompleted(id);
+  }
+
+  editTodo(id, values) {
+    this.model.editTodo(id, values);
+    const row = document.getElementById(id);
+    row.children[0].innerText = values.title;
+    row.children[1].innerText = values.description;
+    row.children[2].children[0].checked = values.completed;
+  }
+
+  removeTodo(id) {
+    this.model.removeTodo(id);
+    document.getElementById(id).remove();
+  }
+
+  createRow(todo) {
+    const row = table.insertRow();
+    row.setAttribute('id', todo.id);
+    row.innerHTML = `
+      <td>${todo.title}</td>
+      <td>${todo.description}</td>
+      <td class="text-center">
+      </td>
+      <td class="text-right">
+      </td>
+    `;
+
+    const checkbox = document.createElement('input');
+    checkbox.type = 'checkbox';
+    checkbox.checked = todo.completed;
+    checkbox.onclick = () => this.toggleCompleted(todo.id);
+    row.children[2].appendChild(checkbox);
+
+    const editBtn = document.createElement('button');
+    editBtn.classList.add('btn', 'btn-primary', 'mb-1');
+    editBtn.innerHTML = '<i class="fa fa-pencil"></i>';
+    editBtn.setAttribute('data-toggle', 'modal');
+    editBtn.setAttribute('data-target', '#modal');
+    editBtn.onclick = () => this.modal.setValues({
+      id: todo.id,
+      title: row.children[0].innerText,
+      description: row.children[1].innerText,
+      completed: row.children[2].children[0].checked,
+    });
+    row.children[3].appendChild(editBtn);
+
+    const removeBtn = document.createElement('button');
+    removeBtn.classList.add('btn', 'btn-danger', 'mb-1', 'ml-1');
+    removeBtn.innerHTML = '<i class="fa fa-trash"></i>';
+    removeBtn.onclick = () => this.removeTodo(todo.id);
+    row.children[3].appendChild(removeBtn);
+  }
+}
